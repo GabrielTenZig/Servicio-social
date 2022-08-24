8e7431006b00e491dfb108ff719157c5f9fba1c8
GabrielTenZig <jesusgabrieltenorioziga@gmail.com>
2022-08-24 14:01:09 -0500

    Sección Proyectos se carga desde el archivo JS.
    - La informacion que aparece en el modal de los Proyectos se carga
        desde el archivo JS donde está dicha información.
    - Falta mejorar los estilos CSS.
    - Falta ingresar más datos al archivo JS (a la espera del profesor).

--- js/proyectos.js
+++ js/proyectos.js
@@ -38,46 +38,64 @@ const mostrar_modal_proyecto = async (id_param) => {
 
     // // Se referencian los elementos del modal
     const elem_nombre = document.getElementsByClassName('modal__nombre')[0]
+    const elem_patrocinador = document.getElementsByClassName('modal__patrocinador')[0]
+    const elem_tipo_patrocinador = document.getElementsByClassName('modal__tipo_patrocinador')[0]
     const elem_estado = document.getElementsByClassName('modal__estado')[0]
-    const elem_container_participantes = document.getElementsByClassName('modal__container_participantes')[0]
-    const elem_introduccion = document.getElementsByClassName('modal__introducción')[0]
-    const elem_container_objetivos = document.getElementsByClassName('modal__container_objetivos')[0]
+    const elem_fecha_inicio = document.getElementsByClassName('modal__fecha_inicio')[0]
     const elem_fecha_finalizacion = document.getElementsByClassName('modal__fecha_finalizacion')[0]
-    const elem_container_articulos = document.getElementsByClassName('modal__container_articulos')[0]
+    const elem_actividades = document.getElementsByClassName('modal__actividades')[0]
+    const elem_container_participantes = document.getElementsByClassName('modal__container_participantes')[0]
+    const elem_lgac_del_ca_asociaas = document.getElementsByClassName('modal__lgac_del_ca_asociadas')[0]
+    const elem_lgac_indivi_asociadas = document.getElementsByClassName('modal__lgac_indivi_asociadas')[0]
+    const elem_container_objetivos = document.getElementsByClassName('modal__container_objetivos')[0]
+    const elem_resumen = document.getElementsByClassName('modal__resumen')[0]
+    const elem_container_producc_academ = document.getElementsByClassName('modal__container_producc_academ')[0]
 
     // // Se limpian los campos del modal
     elem_nombre.textContent = ""
+    elem_patrocinador.textContent = ""
+    elem_tipo_patrocinador.textContent = ""
     elem_estado.textContent = ""
-    elem_container_participantes.innerHTML = ""
-    elem_introduccion.textContent = ""
-    elem_container_objetivos.innerHTML = ""
+    elem_fecha_inicio.textContent = ""
     elem_fecha_finalizacion.textContent = ""
-    elem_container_articulos.innerHTML = ""
+    elem_actividades.textContent = ""
+    elem_container_participantes.innerHTML = ""
+    elem_lgac_del_ca_asociaas.textContent = ""
+    elem_lgac_indivi_asociadas.textContent = ""
+    elem_container_objetivos.innerHTML = ""
+    elem_resumen.textContent = ""
+    elem_container_producc_academ.innerHTML = ""
 
     // // Se llenan los campos del modal
     elem_nombre.textContent = data_proyecto.nombre
+    elem_patrocinador.textContent = data_proyecto.patrocinador
+    elem_tipo_patrocinador.textContent = data_proyecto.tipo_de_patrocinador
     elem_estado.textContent = data_proyecto.estado
+    elem_fecha_inicio.textContent = data_proyecto.fecha_de_inicio
+    elem_fecha_finalizacion.textContent = data_proyecto.fecha_finalizacion
+    elem_actividades.textContent = data_proyecto.actividades
 
-    data_proyecto.participantes.forEach(participante => {
+    data_proyecto.integrantes_del_ca_que_participan.forEach(participante => {
         elem_container_participantes.innerHTML += `
             <li>
                 <a href="${participante.url}" >${participante.nombre}</a>
             </li>`
     })
 
-    elem_introduccion.textContent = data_proyecto.introduccion
-
+    elem_lgac_del_ca_asociaas.textContent = data_proyecto.lgac_del_ca_asociada
+    elem_lgac_indivi_asociadas.textContent = data_proyecto.lgac_de_individuales_asociada
+    
     data_proyecto.objetivos.forEach(objetivo => {
         elem_container_objetivos.innerHTML += `
-            <li>
-                ${objetivo}
-            </li>`
+        <li>
+        ${objetivo}
+        </li>`
     })
 
-    elem_fecha_finalizacion.textContent = data_proyecto.fecha_estimada_finalizacion
+    elem_resumen.textContent = data_proyecto.resumen
     
-    data_proyecto.articulos_escritos.forEach(articulo => {
-        elem_container_articulos.innerHTML += `
+    data_proyecto.produccion_academica_relacionada.forEach(articulo => {
+        elem_container_producc_academ.innerHTML += `
         <li>
             <a href="${articulo.url}" >${articulo.nombre}</a>
         </li>`
